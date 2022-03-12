17955c352485b5b179365aacb626676eefbfe1d1
Xiong (Shawn) He <sxuxxh@yahoo.com>
2022-03-12 15:29:48 -0500

    refactor CheckForEven

--- assignments/section2/CheckForEven.java
+++ assignments/section2/CheckForEven.java
@@ -5,11 +5,6 @@ import genspark.assignments.Assignment;
 public class CheckForEven implements Assignment {
     public String solution(int number) {
         // ↓↓↓↓ your code goes here ↓↓↓↓
-        if (number % 2 ==0) {
-            return "even";
-        }
-        else {
-            return "odd";
-        }
+        return (number % 2 == 0) ? "even" : "odd";
     }
 }
