e9070e40933ba0dfd8282a7f319f308b8b2d59fd
sxuxxh <84204448+sxuxxh@users.noreply.github.com>
2022-05-04 22:23:40 -0400

    Update FactorialComputation.java
    Use Functional Programming

--- assignments/section4/FactorialComputation.java
+++ assignments/section4/FactorialComputation.java
@@ -10,5 +10,18 @@ public class FactorialComputation implements Assignment {
             retNum = retNum * i;
         }
         return retNum;
+        
+        // ==> Functional Programming
+        public static int factorialComputation(int n) {
+          if(n == 0) {
+            return 1;
+          }
+          return (n * factorialComputation(n-1));
+        }
+
+        public int solution(int n) {
+        // ↓↓↓↓ your code goes here ↓↓↓↓
+          return factorialComputation(n);
+        }
     }
 }
